af6b14e5fe31030075d0c042c747017465b4c4c5
kamaleon391 <kamaleon391@gmail.com>
2018-01-10 16:24:53 -0600

    Envio de email con tabla html sin ut8

--- index2018_Estados.php
+++ index2018_Estados.php
@@ -3,7 +3,165 @@
     require 'conexion.php';
 
     $mensaje="<h1>Titulo del mensaje</h1>
-    <p>Cuerpo del mensaje</p>";
+    <table width='150px' align='center' cellspacing='0' border='0' style='font-size: 13px; border: solid 1px gray'>
+      <tr>
+        <tr>
+          <td width='3%'>&nbsp;</td>
+          <td colspan='5'><img src='Logo.jpg' style='width:400px;'></td>
+          <td width='3%'>&nbsp;</td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+         <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td width='49%' style='border-bottom: 1pt solid rgb(230, 230, 230);'> Dir. General</td>";
+
+         $mensaje .= "
+          <td width='6%' style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td width='4%' style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>PEMEX</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Admon. Central</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Ref. Energetica</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Instalaciones</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Petroleo</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Gas</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Petroquimica</td>";
+
+         $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Gasolina</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Ref. Laboral</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Sindicato</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Refinacion</td>";
+
+          $mensaje .= "
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
+        </tr>
+        <tr>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
+          <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Seguridad Industrial</td>";
+
+          $mensaje .= "
+        </tr>
+
+        <td colspan='7' style='text-align: right;'><span style='text-align: right;
+          font-size: 8px;
+          color: rgb(139, 139, 139);'>Monitoreo de prensa 2018</span></td>
+      </tr>
+    </table>
+    <br><br>
+    <div style='text-align: center; font-size: 14px;'>Para mas contenido visite su <a href='https://www.taringa.net'>Sistema de Informacion</a></div>";
 
     echo $mensaje."<br><br>";
 
