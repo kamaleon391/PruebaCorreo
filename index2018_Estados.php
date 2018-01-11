a46505537f9507ea6ea48b1e108b0ea2179b7c77
kamaleon391 <kamaleon391@gmail.com>
2018-01-11 13:06:05 -0600

    Cambio de link de Sistema de informacion

--- index2018_Estados.php
+++ index2018_Estados.php
@@ -39,7 +39,7 @@
           <td width='49%' style='border-bottom: 1pt solid rgb(230, 230, 230);'> Dir. General</td>";
 
           if(numberNotes(5, $fecha))
-            $mensaje .= "<td width='25%'  style='border-bottom: 1pt solid rgb(230, 230, 230);'><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('5'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
+            $mensaje .= "<td width='25%'  style='border-bottom: 1pt solid rgb(230, 230, 230);'><a href='http://187.247.253.5/PruebaCorreo/pemex/exportpemex1.php?p=".base64_encode(base64_encode('5'))."&f=$fecha'>".'Clic aquí'."</a></td>";
           else
             $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";
 
@@ -53,11 +53,6 @@
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>PEMEX</td>";
 
-          if(numberNotes(6, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('6'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
-
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -67,11 +62,6 @@
         <tr>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Admon. Central</td>";
-
-          if(numberNotes(7, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('7'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
 
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -83,11 +73,6 @@
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Ref. Energética</td>";
 
-          if(numberNotes(8, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('8'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
-
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -97,11 +82,6 @@
         <tr>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Instalaciones</td>";
-
-          if(numberNotes(9, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('9'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
 
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -113,11 +93,6 @@
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Petróleo</td>";
 
-          if(numberNotes(10, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('10'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
-
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -127,11 +102,6 @@
         <tr>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Gas</td>";
-
-          if(numberNotes(11, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('11'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
 
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -143,11 +113,6 @@
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Petroquímica</td>";
 
-          if(numberNotes(12, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('12'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
-
          $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -157,11 +122,6 @@
         <tr>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Gasolina</td>";
-
-          if(numberNotes(13, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('13'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
 
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -173,11 +133,6 @@
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Ref. Laboral</td>";
 
-          if(numberNotes(14, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('14'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
-
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -187,11 +142,6 @@
         <tr>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Sindicato</td>";
-
-          if(numberNotes(15, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('15'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
 
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -203,11 +153,6 @@
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Refinación</td>";
 
-          if(numberNotes(16, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('16'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
-
           $mensaje .= "
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);' >&nbsp;</td>
@@ -218,11 +163,6 @@
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>
           <td style='border-bottom: 1pt solid rgb(230, 230, 230);'>Seguridad Industrial</td>";
 
-          if(numberNotes(17, $fecha))
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);' ><a href='http://187.247.253.5/external/services/mail/pemex/exportpemex.php?p=".base64_encode(base64_encode('17'))."&f=$fecha'>".utf8_decode('Clic aquí')."</a></td>";
-          else
-            $mensaje .= "<td style='border-bottom: 1pt solid rgb(230, 230, 230);'>&nbsp;</td>";          
-
           $mensaje .= "
         </tr>
 
@@ -232,7 +172,7 @@
       </tr>
     </table>
     <br><br>
-    <div style='text-align: center; font-size: 14px;'>Para mas contenido visite su <a href='https://www.taringa.net'>Sistema de Informacion</a></div>";
+    <div style='text-align: center; font-size: 14px;'>Para mas contenido visite su <a href='http://www.gaimpresos.com/boards/corpogas'>Sistema de Informacion</a></div>";
 
     echo $mensaje."<br><br>";
 
