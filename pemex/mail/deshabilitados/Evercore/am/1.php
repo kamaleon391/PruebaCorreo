723c113c89e889951f384d269352eb6571823395
kamaleon391 <kamaleon391@gmail.com>
2018-02-18 15:35:33 -0600

    Pruebas en local

--- pemex/mail/deshabilitados/Evercore/am/1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$directorio=opendir("http://mival.mx/pulso/Pdfs/Interactiva/SeccA/FLASH/Files/"); 
-echo "<b>Directorio actual:</b><br>$dir<br>"; 
-echo "<b>Archivos:</b><br>"; 
-while ($archivo = readdir($directorio))
-  echo "$archivo<br>"; 
-closedir($directorio); 
-?>
--- pemex/mail/deshabilitados/Evercore/am/dbconn/verbindung.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysql_hostname = "192.168.1.19";
-$mysql_user = "adconmedios";
-$mysql_password = "conadmediospassx";
-$mysql_database = "monitoreo";
-$con = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Error en la Conexion");
-mysql_select_db($mysql_database, $con) or die("Error en la base de datos");
-?>
